1170d976f7e6e408824d9fb46271f04e106fcac1
diii2409 <duynhut366@gmail.com>
2024-03-26 13:49:43 +0700

    server chat

--- server/index.js
+++ server/index.js
@@ -2,17 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoute = require("./Routes/userRoute.js");
+const chatRoute = require("./Routes/chatRoute.js");
 
 const app = express();
 require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api/users", userRoute);
 
-app.get("/", (req, res) => {
-	res.send("Welcome hello World!");
-});
+app.use("/api/users", userRoute);
+app.use("/api/chats", chatRoute);
 
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
