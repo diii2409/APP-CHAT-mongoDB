de247852d97b5d60be94205cfaea5a3ab6b46a83
diii2409 <duynhut366@gmail.com>
2024-03-26 07:52:14 +0700

    client register and fetch api

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NavBar from "./components/NavBar";
+
+function App() {
+	return (
+		<>
+			<NavBar />
+			<Container>
+				<Routes>
+					<Route path='/' element={<Chat />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/*' element={<Navigate to='/' />} />
+				</Routes>
+			</Container>
+		</>
+	);
+}
+
+export default App;
